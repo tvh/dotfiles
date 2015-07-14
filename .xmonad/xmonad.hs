2ac9dd24ae82ca5ed82079343b0ab4eefb49b11c
Timo von Holtz <tvh@anchor.com.au>
2015-07-14 10:40:06 +1000

    switch to xscreensaver

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -96,10 +96,9 @@ main = do
         , startupHook = do
             spawn "xfce4-power-manager"
             spawnOn "1" "xfce4-terminal -x weechat"
-            spawn "light-locker"
             spawn "hipchat"
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "light-locker-command -l")
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command --lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         , ((mod4Mask .|. shiftMask, xK_t), spawn "msu")
