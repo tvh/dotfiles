0f7227731d1b98380ebab28accbf97fe69c69514
Timo von Holtz <tvh@tvholtz.de>
2016-05-08 20:32:37 +0200

    Simplify xmonad and compile with stack

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,120 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 import Data.Monoid
-import qualified DBus as D
-import qualified DBus.Client as D
-import qualified Codec.Binary.UTF8.String as UTF8
-import Graphics.X11.ExtraTypes.XF86
 import XMonad
-import XMonad.Actions.Volume
-import XMonad.Actions.SpawnOn
-import XMonad.Hooks.DynamicLog
+import XMonad.Config.Kde
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
-import XMonad.Layout.Grid
 import XMonad.Layout.MultiColumns
 import XMonad.Layout.NoBorders
-import XMonad.Layout.PerWorkspace
 import qualified XMonad.StackSet as W -- to shift and float windows
-import XMonad.Util.Dzen
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
-
-alert :: forall a. RealFrac a => a -> X ()
-alert = dzenConfig centered . show . (round :: a -> Int)
-
-centered :: (Int, [String]) -> X (Int, [String])
-centered =
-        onCurr (center 150 66)
-    >=> font "-*-helvetica-*-r-*-*-64-*-*-*-*-*-*-*"
-    >=> addArgs ["-fg", "#80c0ff"]
-    >=> addArgs ["-bg", "#000040"]
-
-toggleMute' :: MonadIO m => m Double
-toggleMute' = do
-    res <- toggleMute
-    case res of
-        False -> getVolume
-        True -> return 0
-
-prettyPrinter :: D.Client -> PP
-prettyPrinter dbus = def
-    { ppOutput   = dbusOutput dbus
-    , ppTitle    = pangoSanitize
-    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
-    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
-    , ppHidden   = pangoColor "white" . wrap "{" "}" . pangoSanitize
-    , ppUrgent   = pangoColor "red"
-    , ppLayout   = pangoColor "purple" . pangoSanitize
-    , ppSep      = " "
-    }
-
-getWellKnownName :: D.Client -> IO ()
-getWellKnownName dbus = do
-  D.requestName dbus (D.busName_ "org.xmonad.Log")
-                [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
-  return ()
-dbusOutput :: D.Client -> String -> IO ()
-dbusOutput dbus str = do
-    let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
-            D.signalBody = [D.toVariant ("<b>" ++ (UTF8.decodeString str) ++ "</b>")]
-        }
-    D.emit dbus signal
-
-pangoColor :: String -> String -> String
-pangoColor fg = wrap left right
-  where
-    left  = "<span foreground=\"" ++ fg ++ "\">"
-    right = "</span>"
-
-pangoSanitize :: String -> String
-pangoSanitize = foldr sanitize ""
-  where
-    sanitize '>'  xs = "&gt;" ++ xs
-    sanitize '<'  xs = "&lt;" ++ xs
-    sanitize '\"' xs = "&quot;" ++ xs
-    sanitize '&'  xs = "&amp;" ++ xs
-    sanitize x    xs = x:xs
 
 main :: IO ()
 main = do
-    dbus <- D.connectSession
-    getWellKnownName dbus
-    spawn "xfce4-panel --disable-wm-check"
-    xmonad $ def
-        { manageHook = manageSpawn <> manageDocks <> manageHook def <> myManageHook
+    xmonad $ kde4Config
+        { manageHook = manageHook kde4Config <> myManageHook
         , layoutHook = smartBorders . avoidStruts $
---                       onWorkspace "1" Grid $
---                       onWorkspace "3" (Tall 1 (3/100) (6/7)) $
-                       (multiCol [1] 0 0.01 (-0.5) ||| layoutHook def)
-        , logHook = do
-            dynamicLogWithPP (prettyPrinter dbus)
-            ewmhDesktopsLogHook
+            (multiCol [1] 0 0.01 (-0.5) ||| layoutHook def)
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         , terminal = "gnome-terminal"
-        , handleEventHook = do
-            fullscreenEventHook
-            ewmhDesktopsEventHook
+        , handleEventHook =
+            handleEventHook kde4Config <> fullscreenEventHook <> ewmhDesktopsEventHook
         , startupHook = do
-            spawn "xfce4-power-manager"
-            spawn "hipchat"
-            spawn "slack"
+            startupHook kde4Config
             ewmhDesktopsStartup
             setWMName "LG3D"
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command --lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        , ((mod4Mask .|. shiftMask, xK_t), spawn "msu")
-        , ((0, xF86XK_AudioLowerVolume), setMute False >> lowerVolume 4 >>= alert)
-        , ((0, xF86XK_AudioRaiseVolume), setMute False >> raiseVolume 4 >>= alert)
-        , ((0, xF86XK_AudioMute), toggleMute' >>= alert)
-        , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -10")
-        , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +10")
-        ]
+        }
 
 myManageHook :: ManageHook
 myManageHook = composeAll . concat $
